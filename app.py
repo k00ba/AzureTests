9c67fb5d54ada48e455b0ae8e28aabd603844c95
Jakub Dabkowski <jakub.dabkowski@netresearch.pl>
2020-10-28 13:43:43 +0100

    New response POST
    Post response for ACS API

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, jsonify, render_template, url_for
+from flask import request
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def home():
-    return jsonify({"status": "aaa"})
+    data = []
+    statuslist = ["yes", "no"]
+    content = request.get_json()
+    for profile in content['data']:
+        status = random.choice(statuslist)
+        item = {"email": profile['email'], "status":status}
+        data.append(item)
+
+    return jsonify({"data":data})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# {"data":[{"email":"jakub.dabkowski+testb2b_1@avaus.com", "status":"tak"},
+# {"email":"jakub.dabkowski+testb2b_2@avaus.com", "status":"nie"}]}
